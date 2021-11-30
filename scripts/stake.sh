c58e96940bb13389b294ded7ca56147f833a2cd9
kevintmax <67485702+kevintmax@users.noreply.github.com>
2021-12-01 01:31:44 +0900

    Update stake.sh

--- scripts/stake.sh
+++ scripts/stake.sh
@@ -9,6 +9,8 @@ if [ ${CURRENT_UNIXTIME} -gt $((${CURRENT_ELECTION_SINCE_UNIXTIME} + 1200)) -a $
         if ~/node.operator.nt/scripts/myElection.sh | grep -q 'ELECTED VALIDATOR'; then
             exit
         else
+            crontab -r
+            sleep $((RANDOM % 600))
             ~/node.operator.nt/scripts/participate.sh
         fi
     fi
