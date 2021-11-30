2a8a41b8a1bc7517ce957d11950b6f60b9711198
kevintmax <67485702+kevintmax@users.noreply.github.com>
2021-12-01 00:56:32 +0900

    Update stake.sh

--- scripts/stake.sh
+++ scripts/stake.sh
@@ -3,8 +3,7 @@
 current_time=$(echo " CURRENT_TIME: ${CURRENT_UNIXTIME};")
 election_start_time=$(echo " ELECTION_START_TIME: ${CURRENT_ELECTION_SINCE_UNIXTIME};")
 election_end_time=$(echo " ELECTION_END_TIME: ${CURRENT_ELECTION_UNTIL_UNIXTIME};")
-
-# Election start time ~ Election end time TRUE
+sleep $(( RANDOM % 600))
 if [ ${CURRENT_UNIXTIME} -gt $((${CURRENT_ELECTION_SINCE_UNIXTIME} + 1200)) -a ${CURRENT_UNIXTIME} -lt ${CURRENT_ELECTION_UNTIL_UNIXTIME} ]; then
     if ~/node.operator.nt/scripts/myElection.sh | grep -q 'SUBMISSION CONFIRMED'; then
         exit
