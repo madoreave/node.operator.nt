3fe741312f9cea7babeab8bdd0f99168d0f605d4
kevintmax <67485702+kevintmax@users.noreply.github.com>
2021-12-01 18:52:32 +0900

    Update submission.sh

--- configs/submission.sh
+++ configs/submission.sh
@@ -1,22 +1,39 @@
+#!/bin/bash
+fuction CHECK_STAKE {
+SUBMISSION_CHECK='50000'
+IN='<<<'
+OUT='>>>'
+NANO='1000000000'
+  n=$(cat ~/node.operator.nt/logs/vas_validator_wallet_001_1 | wc -l) 
+  for (( i = 1; i <= n; i++ ))
+  do
+  CHECK_IN_OUT=$(cat ~/node.operator.nt/logs/vas_validator_wallet_001_1 | awk "FNR == ${i}" | awk '{print $4}' | sed -r 's/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g')
+  CHECK_ELECTOR=$(cat ~/node.operator.nt/logs/vas_validator_wallet_001_1 | awk "FNR == ${i}" | awk '{print $6}')
+  STAKE=$(cat ~/node.operator.nt/logs/vas_validator_wallet_001_1 | awk "FNR == ${i}" | awk '{print $5}'); STAKE=${STAKE%.*}
+    if [ "$CHECK_ELECTOR" = "$ELECTOR" ] && [ "$CHECK_IN_OUT" = "$OUT" ] && [ "$STAKE" -gt "$SUBMISSION_CHECK" ]; then
+         STAKE=$((STAKE * NANO))
+         CHECK_ELECTION_SUBMISSION=$STAKE
+    fi
+}
+
 ELECTOR='Ef8zMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzM0vF'
 CONFIRM_AMOUNT='1.0'
-SUBMISSION_CHECK='50000'
 
 mytonctrl <<< "vas validator_wallet_001" > ~/node.operator.nt/logs/vas_validator_wallet_001
 cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep "minutes ago" > ~/node.operator.nt/logs/vas_validator_wallet_001_1
-cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep "an hour ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
-cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep "2 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
-cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep "3 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
-cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep "4 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
-cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep "5 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
-cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep "6 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
-cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep "7 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
-cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep "8 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
+cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep -w "an hour ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
+cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep -w "2 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
+cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep -w "3 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
+cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep -w "4 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
+cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep -w "5 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
+cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep -w "6 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
+cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep -w "7 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
+cat ~/node.operator.nt/logs/vas_validator_wallet_001 | grep -w "8 hours ago" >> ~/node.operator.nt/logs/vas_validator_wallet_001_1
 
 IN='<<<'
 OUT='>>>'
 n=$(cat ~/node.operator.nt/logs/vas_validator_wallet_001_1 | wc -l) 
-NANO='1000000000'
+
 for (( i = 1; i <= n; i++ ))
 do
 CHECK_IN_OUT=$(cat ~/node.operator.nt/logs/vas_validator_wallet_001_1 | awk "FNR == ${i}" | awk '{print $4}' | sed -r 's/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g')
@@ -31,15 +48,5 @@ fi
 done
 
 if [ "$STAKE_SUBMITTED" == 1 ]; then
-  for (( i = 1; i <= n; i++ ))
-  do
-  CHECK_IN_OUT=$(cat ~/node.operator.nt/logs/vas_validator_wallet_001_1 | awk "FNR == ${i}" | awk '{print $4}' | sed -r 's/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g')
-  CHECK_ELECTOR=$(cat ~/node.operator.nt/logs/vas_validator_wallet_001_1 | awk "FNR == ${i}" | awk '{print $6}')
-  STAKE=$(cat ~/node.operator.nt/logs/vas_validator_wallet_001_1 | awk "FNR == ${i}" | awk '{print $5}') && STAKE=${STAKE%.*}
-    if [ "$CHECK_ELECTOR" = "$ELECTOR" ] && [ "$CHECK_IN_OUT" = "$OUT" ] && [ "$STAKE" -gt "$SUBMISSION_CHECK" ]; then
-         STAKE=$((STAKE * NANO))
-         CHECK_ELECTION_SUBMISSION=$STAKE
-         
-    fi
-  done
+CHECK_STAKE
 fi
