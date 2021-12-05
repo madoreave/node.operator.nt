708c04f121d54e886dd836c4eec86d87882a0240
kevintmax <67485702+kevintmax@users.noreply.github.com>
2021-12-06 08:49:29 +0900

    Update stake.sh

--- scripts/stake.sh
+++ scripts/stake.sh
@@ -4,9 +4,11 @@
 if [ ${CURRENT_UNIXTIME} -gt $((${CURRENT_ELECTION_SINCE_UNIXTIME} + 100)) -a ${CURRENT_UNIXTIME} -lt ${CURRENT_ELECTION_UNTIL_UNIXTIME} ]; then
 
     if ~/node.operator.nt/scripts/myElection.sh | grep -q 'SUBMISSION CONFIRMED'; then
+        mytonctrl <<< "set stake 0"
         exit
     else
         if ~/node.operator.nt/scripts/myElection.sh | grep -q 'ELECTED VALIDATOR'; then
+            mytonctrl <<< "set stake 0"
             exit
         else
             sleep $((RANDOM % 300))
@@ -14,4 +16,4 @@ if [ ${CURRENT_UNIXTIME} -gt $((${CURRENT_ELECTION_SINCE_UNIXTIME} + 100)) -a ${
         fi
     fi
 fi
-sleep 5
+
