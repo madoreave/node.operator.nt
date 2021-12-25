d2c122164a8e79a5579bfaaf9b65c592ddf455da
kevintmax <67485702+kevintmax@users.noreply.github.com>
2021-12-25 18:35:43 +0900

    Update alarm.py

--- monitor/alarm.py
+++ monitor/alarm.py
@@ -23,10 +23,12 @@ VALIDATION = subprocess.check_output("tail -n 1 ~/node.operator.nt/logs/election
 ELECTION = subprocess.check_output("tail -n 1 ~/node.operator.nt/logs/election.log | grep -oP '(?<=ELECTION_STATUS: )[0-9]'",shell=True);
 ERROR_TIME_UNIX = subprocess.check_output("tail -n 1 ~/node.operator.nt/logs/election.log | grep -oP '(?<=CURRENT_TIME).*' | awk '{print $2}' | tr -d ';'",shell=True);
 SYNC=subprocess.check_output("~/node.operator.nt/scripts/checkSync.sh| grep TIME_DIFF | awk '{print $4}' | tr '[:upper:]' '[:lower:]'",shell=True);
+DISKSPACE = subprocess.check_output("tail -n 1 ~/node.operator.nt/logs/election.log | grep -oP '(?<=DISKSPACE: )[0-9]'",shell=True);
 
 ERROR_TIME = datetime.utcfromtimestamp(int(ERROR_TIME_UNIX)).strftime('%Y-%m-%d %H:%M:%S')
 SYNCalarm = "-100"
 SYNCalarm1 = 0
+DISKalarm = 50
 
 if int(VALIDATION) == 0:
   bot.sendMessage(chat_id='-1001685894260', text=str(HOSTNAME)+" Not Validating "+str(ERROR_TIME));
@@ -36,3 +38,6 @@ if int(SYNC) < int(SYNCalarm):
   bot.sendMessage(chat_id='-1001685894260', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
 if int(SYNC) > int(SYNCalarm1):
   bot.sendMessage(chat_id='-1001685894260', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
+if int(DISKSPACE) > in(DISKalarm):
+  bot.sendMessage(chat_id='-1001685894260', text=str(HOSTNAME)+" DISK SPACE FULL, DISK SPACE:"+str(DISKSPACE));
+
